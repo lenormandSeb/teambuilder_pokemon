4d0ffd3440c0687da2441dbda65bddefdf027634
lenormandS <lenormand.sbn@gmail.com>
2020-07-14 00:02:35 +0200

    wip refacto add pokemon

---
+++ public/js/statChanger.js
@@ -0,0 +1,48 @@
+class Test {
+    constructor(hp, atk, def, atkspe, defspe, vit) {
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+        this.atkspe = atkspe;
+        this.defspe = defspe;
+        this.vit = vit;
+    };
+    calculateur() {
+        $('#hpfinal').html(this.PVCalculator(this.hp, 50, 0, 31))
+        $('#atkfinal').html(this.StatCalculator(this.atk, 50, 0, 31))
+        $('#deffinal').html(this.StatCalculator(this.def, 50, 0, 31))
+        $('#atkspefinal').html(this.StatCalculator(this.atkspe, 50, 0, 31))
+        $('#defspefinal').html(this.StatCalculator(this.defspe, 50, 0, 31))
+        $('#vitfinal').html(this.StatCalculator(this.vit, 50, 0, 31))
+    };
+    update(text) {
+        if ($(text).attr('name') === 'hpEV' || $(text).attr('name') === 'hpIV') {
+            var newstat = this.PVCalculator(this.hp, 100, parseInt($('.hpEV').val()), parseInt($('.hpIV').val()))
+            $('#hpfinal').html(newstat);
+        } else {
+            if ($(text).attr('name') == 'atkEV' || $(text).attr('name') == 'atkIV') {
+                console.log('atk')
+            }
+            else if ($(text).attr('name') === 'defEV' || $(text).attr('name') == 'defIV') {
+                console.log('def')
+            }
+            else if ($(text).attr('name') === 'atkspeEV' || $(text).attr('name') == 'atkspeIV') {
+
+            }
+            else if ($(text).attr('name') === 'defspeEV' || $(text).attr('name') == 'defspeIV') {
+
+            }
+            else if ($(text).attr('name') === 'vitEV' || $(text).attr('name') == 'vitIV') {
+
+            }
+        }
+    };
+    StatCalculator(stat, niv, ev, iv) {
+        var calc = ((iv + 2 * stat + Math.floor(ev / 4)) * (niv / 100) + 5) * 1
+        return Math.floor(calc)
+    };
+    PVCalculator(hp, niv, ev, iv) {
+        var calc = (iv + 2 * hp + Math.floor(ev / 4)) * (niv / 100) + 10 + niv
+        return Math.floor(calc)
+    }
+}
--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -7,24 +7,429 @@ use App\Entity\Type;
 use App\Entity\Talent;
 
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class PokemonController extends AbstractController
 {
     /**
-     * @Route("/pokemon/", name="pokemon")
+     * @Route("/pokemon", name="pokemon")
      */
-    public function getAllPokemon(Request $request)
+    public function index(Request $request, EntityManagerInterface $entityManager)
     {
-        try {
-            $pr = $this->getDoctrine()->getRepository(Pokemon::class);
-            $pokemons = $pr->findAll();
-            return new Response(serialize($pokemons), 200, ['Content-Type' => 'application/json']);
-        } catch(\Exception $ex) {
-            return $ex->getMessage();
+        $pokemon = new Pokemon();
+
+        $type = $this->getDoctrine()->getRepository(Type::class);
+        $talent = $this->getDoctrine()->getRepository(Talent::class);
+        $repositoryPokemon = $this->getDoctrine()->getRepository(Pokemon::class);
+
+        $allType = $type->findAll();
+        $allTalent = $talent->findAll();
+        $pokemons = $repositoryPokemon->findAllByNumDex();
+
+        $form = $this->createFormBuilder()
+                    ->add('Nom', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('Numero', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('type_1', ChoiceType::class,[
+                        'choices' => [$allType],
+                        'choice_label' => function(Type $allType, $key, $value) {
+                            return $allType->getName();
+                        },
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('type_2', ChoiceType::class,[
+                        'choices' => [$allType],
+                        'choice_label' => function(Type $allType, $key, $value) {
+                            return $allType->getName();
+                        },
+                        'required' => false,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('talent_1', ChoiceType::class,[
+                        'choices' => [$allTalent],
+                        'choice_label' => function(Talent $allTalent, $key, $value) {
+                            return $allTalent->getName();
+                        },
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('talent_2', ChoiceType::class,[
+                        'choices' => [$allTalent],
+                        'choice_label' => function(Talent $allTalent, $key, $value) {
+                            return $allTalent->getName();
+                        },
+                        'required' => false,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('talent_cache', ChoiceType::class,[
+                        'choices' => [$allTalent],
+                        'choice_label' => function(Talent $allTalent, $key, $value) {
+                            return $allTalent->getName();
+                        },
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('generation', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('HP', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('ATK', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('DEF', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('SPE_ATK', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('SPE_DEF', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('SPEED', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('Valider', SubmitType::class,[
+                        'attr' => 
+                            ['class' => 'btn btn-success']
+                        ]
+                    )
+                    ->add('Annuler', ResetType::class,[
+                        'attr' => 
+                            ['class' => 'btn btn-danger']
+                    ])
+                    ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $po = $form->getData();
+
+            $okemon = new Pokemon();
+            $pokemon->setName($po['Nom']);
+            $pokemon->setNumDex($po['Numero']);
+            $pokemon->setTypeOne($po['type_1']->getId());
+            $pokemon->setTalentOne($po['talent_1']->getId());
+            $pokemon->setTalentHide($po['talent_cache']->getId());
+            $pokemon->setHp($po['HP']);
+            $pokemon->setAttack($po['ATK']);
+            $pokemon->setDefense($po['DEF']);
+            $pokemon->setSpeAttack($po['SPE_ATK']);
+            $pokemon->setSpeDefense($po['SPE_DEF']);
+            $pokemon->setSpeed($po['SPEED']);
+            $pokemon->setGeneration($po['generation']);
+
+            if (is_null($po['talent_2']))
+            {
+                $pokemon->setTalentTwo(0);
+            }else {
+                $pokemon->setTalentTwo($po['talent_2']->getId());
+            }
+
+            if (is_null($po['type_2']))
+            {
+                $pokemon->setTypeTwo(0);
+            }else {
+                $pokemon->setTypeTwo($po['type_2']->getId());
+            }
+
+            $entityManager->persist($pokemon);
+
+            $entityManager->flush();
+            return $this->redirectToRoute('app_admin_pokemon'); 
         }
+
+        return $this->render('pokemon/index.html.twig', [
+            'controller_name' => 'PokemonController',
+            'form' => $form->createView(),
+            'pokemons' => $pokemons
+        ]);
+    }
+
+    /**
+     * @Route("/pokemon/add", name="pokemon-add")
+     */
+    public function ajout(Request $request)
+    {
+        $pokemon = new Pokemon();
+
+        $type = $this->getDoctrine()->getRepository(Type::class);
+        $talent = $this->getDoctrine()->getRepository(Talent::class);
+        $repositoryPokemon = $this->getDoctrine()->getRepository(Pokemon::class);
+
+        $allType = $type->findAll();
+        $allTalent = $talent->findAll();
+        $pokemons = $repositoryPokemon->findAll();
+
+        $form = $this->createFormBuilder()
+                    ->add('Nom', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('Numero', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('type_1', ChoiceType::class,[
+                        'choices' => [$allType],
+                        'choice_label' => function(Type $allType, $key, $value) {
+                            return $allType->getName();
+                        },
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('type_2', ChoiceType::class,[
+                        'choices' => [$allType],
+                        'choice_label' => function(Type $allType, $key, $value) {
+                            return $allType->getName();
+                        },
+                        'required' => false,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('talent_1', ChoiceType::class,[
+                        'choices' => [$allTalent],
+                        'choice_label' => function(Talent $allTalent, $key, $value) {
+                            return $allTalent->getName();
+                        },
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('talent_2', ChoiceType::class,[
+                        'choices' => [$allTalent],
+                        'choice_label' => function(Talent $allTalent, $key, $value) {
+                            return $allTalent->getName();
+                        },
+                        'required' => false,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('talent_cache', ChoiceType::class,[
+                        'choices' => [$allTalent],
+                        'choice_label' => function(Talent $allTalent, $key, $value) {
+                            return $allTalent->getName();
+                        },
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('generation', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('HP', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('ATK', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('DEF', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('SPE_ATK', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('SPE_DEF', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('SPEED', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('Valider', SubmitType::class,[
+                        'attr' => 
+                            ['class' => 'btn btn-success']
+                        ]
+                    )
+                    ->add('Annuler', ResetType::class,[
+                        'attr' => 
+                            ['class' => 'btn btn-danger']
+                    ])
+                    ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $po = $form->getData();
+
+            $talent = $type = $this->getDoctrine()->getRepository(Type::class);
+
+            $type1 = $talent->find($po['type_1']->getId());
+            $pokemon = new Pokemon();
+            $pokemon->setName($po['Nom']);
+            $pokemon->setNumDex($po['Numero']);
+            $pokemon->setTypeOne($type1);
+            $pokemon->setHp($po['HP']);
+            $pokemon->setAttack($po['ATK']);
+            $pokemon->setDefense($po['DEF']);
+            $pokemon->setSpeAttack($po['SPE_ATK']);
+            $pokemon->setSpeDefense($po['SPE_DEF']);
+            $pokemon->setSpeed($po['SPEED']);
+            $pokemon->setGeneration($po['generation']);
+
+            if ($po['type_2'] != null) {
+                $type2 = $talent->find($po['type_2']->getId());
+                $pokemon->setTypeTwo($type2);
+            }
+
+            $entityManager->persist($pokemon);
+
+            $entityManager->flush();
+            return $this->redirectToRoute('app_admin_pokemon'); 
+        }
+
+        return $this->render('modal/pokemon-add.html.twig', [
+            'controller_name' => 'PokemonController',
+            'form' => $form->createView(),
+            'pokemons' => $pokemons
+        ]);
+    }
+
+    /**
+     * @Route("/pokemon/{id}", name="pokemon-modification")
+     */
+    public function modification(Request $request, $id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Pokemon::class);
+        $type = $this->getDoctrine()->getRepository(Type::class);
+        $talent = $this->getDoctrine()->getRepository(Talent::class);
+        $allType = $type->findAll();
+        $allTalent = $talent->findAll();
+        $pokemon = $repository->find($id);
+
+
+        $form = $this->createFormBuilder($pokemon)
+                    ->add('name', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('numDex', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('typeone', ChoiceType::class,[
+                        'choices' => [$allType],
+                        'choice_label' => function(Type $allType, $key, $value) {
+                            return $allType->getName();
+                        },
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('typetwo', ChoiceType::class,[
+                        'choices' => [$allType],
+                        'choice_label' => function(Type $allType, $key, $value) {
+                            return $allType->getName();
+                        },
+                        'required' => false,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('talentone', ChoiceType::class,[
+                        'choices' => [$allTalent],
+                        'choice_label' => function(Talent $allTalent, $key, $value) {
+                            return $allTalent->getName();
+                        },
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('talenttwo', ChoiceType::class,[
+                        'choices' => [$allTalent],
+                        'choice_label' => function(Talent $allTalent, $key, $value) {
+                            return $allTalent->getName();
+                        },
+                        'required' => false,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('talenthide', ChoiceType::class,[
+                        'choices' => [$allTalent],
+                        'choice_label' => function(Talent $allTalent, $key, $value) {
+                            return $allTalent->getName();
+                        },
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('generation', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('HP', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('attack', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('defense', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('speattack', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('spedefense', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('SPEED', TextType::class, [
+                        'required' => true,
+                        'attr' => ['class' => 'form-control']
+                    ])
+                    ->add('Valider', SubmitType::class,[
+                        'attr' => 
+                            ['class' => 'btn btn-success']
+                        ]
+                    )
+                    ->add('Annuler', ResetType::class,[
+                        'attr' => 
+                            ['class' => 'btn btn-danger']
+                    ])
+                    ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() /* && $form->isValid() */)
+        {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->flush();
+
+            return $this->redirectToRoute('app_admin_pokemon');
+        }
+        return $this->render('modal/pokemon.html.twig',[
+            'form' => $form->createView(),
+            'pokemon' => $pokemon
+        ]);
     }
 }
